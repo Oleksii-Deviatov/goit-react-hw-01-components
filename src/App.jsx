084a3ce5e426cfbddec4a58bb66291b4f107f745
Alexeii Deviatov <65559135+lewyk@users.noreply.github.com>
2021-02-17 23:49:46 +0200

    StatistiscSection

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import statisticalData from "./data/statistical_data.json";
 const App = () => (
   <div className="app">
     <SocialProfile user={user} />
-    {/* <StatistiscSection title="Upload stats" stats={statisticalData} /> */}
-    {/* <StatistiscSection stats123={statisticalData} /> */}
+    <StatistiscSection title="Upload stats" stats={statisticalData} />
+    <StatistiscSection stats={statisticalData} />
   </div>
 );
 
