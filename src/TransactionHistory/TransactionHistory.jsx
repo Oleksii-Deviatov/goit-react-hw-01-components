0361b0425452d987d4ed56fa177d717211c86b43
Alexeii Deviatov <65559135+lewyk@users.noreply.github.com>
2021-02-19 15:59:41 +0200

    fix TransactionHistory

--- src/TransactionHistory/TransactionHistory.jsx
+++ src/TransactionHistory/TransactionHistory.jsx
@@ -3,27 +3,25 @@ import PropTypes from "prop-types";
 import TransactionHistoryItem from "./TransactionHistoryItem";
 
 const TransactionHistory = ({ items }) => (
-  <>
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map(({ id, type, amount, currency }) => (
-          <TransactionHistoryItem
-            key={id}
-            type={type}
-            amount={amount}
-            currency={currency}
-          />
-        ))}
-      </tbody>
-    </table>
-  </>
+  <table className="transaction-history">
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
+    <tbody>
+      {items.map(({ id, type, amount, currency }) => (
+        <TransactionHistoryItem
+          key={id}
+          type={type}
+          amount={amount}
+          currency={currency}
+        />
+      ))}
+    </tbody>
+  </table>
 );
 
 TransactionHistory.propTypes = {
--- src/TransactionHistory/TransactionHistoryItem.jsx
+++ src/TransactionHistory/TransactionHistoryItem.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const TransactionHistoryItem = ({ type, amount, currency }) => (
-  <>
-    <tr>
-      <td>{type}</td>
-      <td>{amount}</td>
-      <td>{currency}</td>
-    </tr>
-  </>
+  <tr>
+    <td>{type}</td>
+    <td>{amount}</td>
+    <td>{currency}</td>
+  </tr>
 );
 
 TransactionHistoryItem.propTypes = {
